684dce669e50489e4e571b5e49be11c6ead231d0
jimatlang <jim.stewart@thelangschool.org>
2024-02-12 13:01:27 -0500

    created Main and Score

---
+++ Score.java
@@ -0,0 +1,20 @@
+public class Score {
+    private int score;
+    private String color;
+    public Score(int score, String color) {
+        this.score = score;
+        this.color = color;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
