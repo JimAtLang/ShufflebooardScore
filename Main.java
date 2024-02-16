754a5a3f2e3c8296bc8bdbc0a78104a0b4136ef9
jimatlang <jim.stewart@thelangschool.org>
2024-02-16 10:37:14 -0500

    created main method

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        
+        new Main();
+    }
+    public Main(){
+        ArrayList<Integer> bluePucks = new ArrayList<>(Arrays.asList(4,7,8,9));
+        ArrayList<Integer> redPucks = new ArrayList<>(Arrays.asList(8,10,13,17));
+        System.out.println(new Score(bluePucks, redPucks));
     }
 }
