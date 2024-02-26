54cba2621541e335a21b954fe5b0796af67e1c5a
Augster69 <august.cornfeld@students.thelangschool.org>
2024-02-26 13:53:51 -0500

    +SCORING,+Long&ShortFOULLINE check

--- Score.java
+++ Score.java
@@ -13,7 +13,59 @@ public class Score {
     public Score(ArrayList<Integer> bluePucks, ArrayList<Integer> redPucks){
         this.score = 0;
         this.color = "";
-        //write code to find score here
+
+        int longestBlue = 0;
+        int longestRed=0;
+        for(int i=0; i<4; i++){
+            int bluePuck=bluePucks.get(i);
+            if(longestBlue<bluePuck && bluePuck>SHORTFOULLINE && bluePuck<LONGFOULLINE){
+                longestBlue=bluePuck;
+            }
+            int redPuck=redPucks.get(i);
+            if(longestRed<redPuck && redPuck>SHORTFOULLINE && redPuck<LONGFOULLINE){
+                longestRed=redPuck;
+            }
+            System.out.println("red="+longestRed+" blue="+longestBlue);
+        }
+        if(longestBlue>longestRed){
+            this.color="blue";
+            for(int puck: bluePucks){
+                if(puck>longestRed && puck<LONGFOULLINE){
+                    if(puck>SHORTFOULLINE){
+                        score++;
+                        continue;
+                    }
+                    if(puck>TWOPOINTLINE){
+                        score++;
+                        
+                    }
+                    if(puck>THREEPOINTLINE){
+                        score++;
+                    }
+                }
+            }
+        }
+        if(longestBlue==longestRed){
+            this.color=null;
+        } else{
+            this.color="red";
+            for(int puck: redPucks){
+                if(puck>longestBlue && puck<LONGFOULLINE){
+                    if(puck>SHORTFOULLINE){
+                        score++;
+                        continue;
+                    }
+                    if(puck>TWOPOINTLINE){
+                        score++;
+                        
+                    }
+                    if(puck>THREEPOINTLINE){
+                        score++;
+                    }
+                }
+            }
+        }
+
     }
     public int getScore() {
         return score;
