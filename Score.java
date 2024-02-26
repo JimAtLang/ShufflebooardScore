ed9fc4fdd723f1cd68edef53a905c88c7e9da245
jimatlang <jim.stewart@thelangschool.org>
2024-02-26 13:53:27 -0500

    finished score constructor I think

--- Score.java
+++ Score.java
@@ -4,7 +4,7 @@ public class Score {
     public static final int TWOPOINTLINE = 10;
     public static final int THREEPOINTLINE = 14;
     public static final int LONGFOULLINE = 16;
-        private int score;
+    private int score;
     private String color;
     public Score(int score, String color) {
         this.score = score;
@@ -17,18 +17,53 @@ public class Score {
         int longestRed = 0;
         for(int i=0;i<4;i++){
             int bluePuck = bluePucks.get(i);
-            if (longestBlue<bluePuck){
+            if (longestBlue<bluePuck && bluePuck > SHORTFOULLINE && longestBlue < LONGFOULLINE){
                 longestBlue = bluePuck;
             }
             int redPuck = redPucks.get(i);
-            if(longestRed<redPuck){
+            if(longestRed<redPuck && redPuck > SHORTFOULLINE  && longestRed < LONGFOULLINE){
                 longestRed = redPuck;
             }
         }
-        System.out.println("Longest blue: " + longestBlue + " Longest red: " + longestRed);
+        // System.out.println("Longest blue: " + longestBlue + " Longest red: " + longestRed);
+        if(longestBlue>longestRed){
+            this.color = "blue";
+            for(int puck : bluePucks){
+                if(puck > longestRed && puck < LONGFOULLINE){
+                    if(puck > SHORTFOULLINE){
+                        score++;
+                    }
+                    if(puck > TWOPOINTLINE){
+                        score++;
+                    }
+                    if(puck > THREEPOINTLINE){
+                        score++;
+                    }
+                }
+            }
+        } else if (longestBlue == longestRed) {
+            this.color = "";
+        } else {
+            this.color = "red";
+            for(int puck : redPucks){
+                if(puck > longestBlue && puck < LONGFOULLINE){
+                    if(puck > SHORTFOULLINE){
+                        score++;
+                    }
+                    if(puck > TWOPOINTLINE){
+                        score++;
+                    }
+                    if(puck > THREEPOINTLINE){
+                        score++;
+                    }
+                }
+            }
+        }
+
 
         //write code to find score here
     }
+    
     public int getScore() {
         return score;
     }
